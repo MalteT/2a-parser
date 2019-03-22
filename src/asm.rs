c036dddff6a232310380be73586dd18d0b072d58
Malte Tammena <malte.tammena@gmx.de>
2019-03-22 17:01:17 +0100

    Add instruction to parser and struct for testing

--- src/asm.rs
+++ src/asm.rs
@@ -1,3 +1,13 @@
+use either::Either;
+
+type Line = Either<Instruction, Label>;
+type Comment = String;
+type Label = String;
+type Address = u8;
+type Byte = u8;
+type Word = u16;
+
+mod display;
 
 /// The different stack sizes the Stack may have.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -27,9 +37,42 @@ pub enum Register {
     R3
 }
 
-/// Possible instructions for the assembler.
+/// Memory address.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum MemAddress {
+    /// Referencing a Label.
+    Label(Label),
+    /// Direct address in memory.
+    Address(Address),
+}
+
+/// Possible instructions for the assembler.
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Instruction {
-    ///
-    EmptyLine,
+    /// Set program origin.
+    AsmOrigin(Address),
+    /// Define byte.
+    AsmByte(Byte),
+    /// Define multiple bytes.
+    AsmDefineBytes(Vec<Byte>),
+    /// Define multiple words.
+    AsmDefineWords(Vec<Word>),
+    /// Make label equivalent to byte.
+    AsmEquals(Label, Byte),
+    /// Define stacksize.
+    AsmStacksize(Stacksize),
+    /// Clear the register.
+    Clear(Register),
+    /// Increase the register by 1.
+    Increase(Register),
+    /// Store the register in memory.
+    Store(MemAddress, Register),
+    /// Relative jump to position.
+    JumpRelative(MemAddress),
+}
+
+/// Represenation of a Minirechner2a ASM program.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Asm {
+    pub lines: Vec<(Line, Comment)>,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! # Minirechner2a ASM parser
+//!
+//! Crate to parse assembly programs into a usable [structure](Asm).
 
 pub mod asm;
 pub mod parser;
