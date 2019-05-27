4908a34dd38ae97ba190e821557fda0f7d7bf315
Malte Tammena <malte.tammena@gmx.de>
2019-05-27 20:03:45 +0200

    Add usable binary
    Signed-off-by: Malte Tammena <malte.tammena@gmx.de>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
 use mr2a_asm_parser::parser::AsmParser;
 
-const PROGRAM: &str = include_str!("../programs/03-SS17-5-a2c.asm");
+use std::env::args;
+use std::fs::read_to_string;
+
+enum Return {
+    Valid,
+    Invalid,
+    FileNotFound,
+}
+
+fn run() -> Return {
+    let args = args().skip(1);
+    let mut ret = Return::Valid;
+
+    for arg in args {
+        match read_to_string(arg.clone()) {
+            Ok(content) => {
+                let parsed = AsmParser::parse(&content);
+                match parsed {
+                    Ok(_) => ret = Return::Valid,
+                    Err(e) => {
+                        eprintln!("\n--> {}:", arg);
+                        eprintln!("{}\n", e);
+                        ret = Return::Invalid;
+                    }
+                }
+            }
+            Err(_) => {
+                eprintln!("'{}': Reading failed", arg);
+                ret = Return::FileNotFound;
+            }
+        }
+    }
+
+    ret
+}
 
 fn main() {
-    let parsed = AsmParser::parse(PROGRAM);
-
-    match parsed {
-        Ok(parsed) => {
-            let s = format!("{}", parsed);
-            println!("{}", s);
-        }
-        Err(e) => println!("{}", e),
-    }
+    std::process::exit(match run() {
+        Return::Valid => 0,
+        Return::Invalid => 1,
+        Return::FileNotFound => 2,
+    })
 }
