8f280161e92bfd71a87d6aaaf288ad57b7654d22
Malte Tammena <malte.tammena@gmx.de>
2019-06-08 16:47:56 +0200

    Update unit tests to reflect newest grammar changes

--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -61,10 +61,53 @@ fn test_ws() {
     use Rule::ws;
     parse!(ws, " ");
     parse!(ws, "\t");
-    parse!(ws, " \t  \t");
     parse_err!(ws, "");
     parse_err!(ws, "_");
 }
+
+#[test]
+fn test_space() {
+    use Rule::space;
+    parse!(space, " \t");
+    parse!(space, "  \t _", "  \t ");
+    parse_err!(space, "");
+}
+
+// TODO: test_sep_ip
+
+#[test]
+fn test_sep_pp() {
+    use Rule::sep_pp;
+    parse!(sep_pp, ", \t");
+    parse!(sep_pp, ",\t    ");
+    parse_err!(sep_pp, " ,");
+    parse_err!(sep_pp, " \t");
+}
+
+// ======================================================================
+
+#[test]
+fn test_oparen() {
+    use Rule::oparen;
+    parse!(oparen, "(");
+    parse_err!(oparen, "{");
+}
+
+#[test]
+fn test_cparen() {
+    use Rule::cparen;
+    parse!(cparen, ")");
+    parse_err!(cparen, "]");
+}
+
+#[test]
+fn test_plus() {
+    use Rule::plus;
+    parse!(plus, "+");
+    parse_err!(plus, "-");
+}
+
+// ======================================================================
 
 #[test]
 fn test_constant_bin() {
@@ -72,7 +115,7 @@ fn test_constant_bin() {
     parse!(constant_bin, "0b10010");
     parse!(constant_bin, "0b0");
     parse!(constant_bin, "0b11111111");
-    parse!(constant_bin, "0b100000000");
+    parse!(constant_bin, "0b100000000", "0b10000000");
     parse_err!(constant_bin, "0x10");
     parse_err!(constant_bin, "10");
     parse_err!(constant_bin, "0b2");
@@ -84,8 +127,8 @@ fn test_constant_hex() {
     parse!(constant_hex, "0xFF");
     parse!(constant_hex, "0xf0");
     parse!(constant_hex, "0x0");
-    parse!(constant_hex, "0x123456789");
-    parse!(constant_hex, "0xabcdef");
+    parse!(constant_hex, "0x123456789", "0x12");
+    parse!(constant_hex, "0xaBcdef", "0xaB");
     parse!(constant_hex, "0xffg", "0xff");
     parse_err!(constant_hex, "ff");
     parse_err!(constant_hex, "10");
@@ -95,9 +138,15 @@ fn test_constant_hex() {
 #[test]
 fn test_constant_dec() {
     use Rule::constant_dec;
-    parse!(constant_dec, "0");
-    parse!(constant_dec, "10");
-    parse!(constant_dec, "011", "011");
+    for i in 0..255 {
+        let x = format!("{}", i);
+        parse!(constant_dec, &x);
+    }
+    for i in 256..10000 {
+        let x = format!("{}", i);
+        parse_err!(constant_dec, &x);
+    }
+    parse!(constant_dec, "0000000000011");
     parse!(constant_dec, "0xff", "0");
     parse!(constant_dec, "0b10", "0");
     parse!(constant_dec, "0123456789", "0123");
@@ -116,18 +165,6 @@ fn test_constant() {
 }
 
 #[test]
-fn test_raw_label() {
-    use Rule::raw_label;
-    parse!(raw_label, "abcdefghijklmnopqrstuvwxyz");
-    parse!(raw_label, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    parse!(raw_label, "0123456789");
-    parse!(raw_label, "abc0123456789def");
-    parse_err!(raw_label, "RLDK0JF");
-    parse_err!(raw_label, "PCLD0KJF");
-    parse_err!(raw_label, "SPLD0KJF");
-}
-
-#[test]
 fn test_rest() {
     use Rule::rest;
     parse!(rest, "sdlkfj sldkj3k j23lk4 j2l3kj szx/c.,76%&^%()");
@@ -138,7 +175,20 @@ fn test_rest() {
 }
 
 #[test]
-fn test_stacksize() {
+fn test_raw_label() {
+    use Rule::raw_label;
+    parse!(raw_label, "abcdefghijklmnopqrstuvwxyz");
+    parse!(raw_label, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    parse!(raw_label, "_0123456789");
+    parse!(raw_label, "abc0123456789def");
+    parse_err!(raw_label, "RLDK0JF");
+    parse_err!(raw_label, "PCLD0KJF");
+    parse_err!(raw_label, "SPLD0KJF");
+    parse_err!(raw_label, "0test");
+}
+
+#[test]
+fn test_raw_stacksize() {
     use Rule::raw_stacksize;
     parse!(raw_stacksize, "16");
     parse!(raw_stacksize, "32");
@@ -163,16 +213,71 @@ fn test_register() {
     parse!(register, "R2");
     parse!(register, "r3");
     parse!(register, "R3");
+    parse!(register, "PC");
     parse_err!(register, "R4");
     parse_err!(register, "r4");
     parse_err!(register, "r");
 }
 
 #[test]
+fn test_registerdi() {
+    use Rule::registerdi;
+    parse!(registerdi, "(r0+)");
+    parse!(registerdi, "(R3+)");
+    parse_err!(registerdi, "(R4+)");
+    parse_err!(registerdi, "(r4)");
+    parse_err!(registerdi, "r2");
+}
+
+#[test]
+fn test_registerddi() {
+    use Rule::registerddi;
+    parse!(registerddi, "((r2+))");
+    parse!(registerddi, "((R1+))");
+    parse_err!(registerddi, "((R+))");
+    parse_err!(registerddi, "((+))");
+    parse_err!(registerddi, "(R2+)");
+}
+
+#[test]
+fn test_memory() {
+    use Rule::memory;
+    parse!(memory, "(0x33)");
+    parse!(memory, "(PC)");
+    parse!(memory, "(_cool)");
+    parse_err!(memory, "PC");
+    parse_err!(memory, "(R2");
+}
+
+#[test]
+fn test_source() {
+    use Rule::source;
+    parse!(source, "R2");
+    parse!(source, "(R1)");
+    parse!(source, "(R2+)");
+    parse!(source, "((R2+))");
+    parse!(source, "(_uncool)");
+    parse!(source, "13");
+}
+
+#[test]
+fn test_destination() {
+    use Rule::destination;
+    parse!(destination, "R2");
+    parse!(destination, "(R1)");
+    parse!(destination, "(R2+)");
+    parse!(destination, "((R2+))");
+    parse!(destination, "(_uncool)");
+    parse!(destination, "13");
+}
+
+// ======================================================================
+
+#[test]
 fn test_comment() {
     use Rule::comment;
-    parse!(comment, "; a comment", "; a comment");
-    parse!(comment, ";comment\tcomment", ";comment\tcomment");
+    parse!(comment, "; a comment");
+    parse!(comment, ";comment\tcomment");
     parse!(comment, ";comment\ntest", ";comment");
     parse!(comment, ";comment\r\ntest", ";comment");
     parse!(comment, ";comment\rtest", ";comment");
@@ -221,26 +326,25 @@ fn test_byte() {
 fn test_db() {
     use Rule::db;
     parse!(db, ".DB 20, 0xff, 0b11110");
-    parse!(db, ".db 100\t,0xf\t ,\t\t0b1");
-    parse!(db, ".db 100\t0xf\t ,\t\t0b1", ".db 100");
+    parse!(db, ".db 100,\t0xf,\t \t\t0b1");
+    parse!(db, ".db 100,0xf ,0b1", ".db 100,0xf");
 }
 
 #[test]
 fn test_dw() {
     use Rule::dw;
     parse!(dw, ".DW 20, 0xff, 0b11110");
-    parse!(dw, ".dw 100\t,0xf\t ,\t\t0b1");
-    parse!(dw, ".dW 100\t0xf\t ,\t\t0b1", ".dW 100");
+    parse!(dw, ".dw 100,\t0xf,\t \t\t0b1");
+    parse!(dw, ".dW 100,0xf ,0b1", ".dW 100,0xf");
 }
 
 #[test]
 fn test_equ() {
     use Rule::equ;
-    parse!(equ, ".EQU label 0xf");
-    parse!(equ, ".equ test 10");
+    parse!(equ, ".EQU label, 0xf");
+    parse!(equ, ".equ test, 10");
+    parse!(equ, ".equ _rest, 0xff");
     parse_err!(equ, ".eq rest 10");
-    parse_err!(equ, ".equ test, 20");
-    parse_err!(equ, ".equ rest, 0xff");
     parse_err!(equ, ".equ 0xff");
     parse_err!(equ, ".equ label");
 }
